b47314e7c229b58d3cbb71f35973f5663546da62
Emma Smith Zbarsky <esmithzb@mathworks.com>
2026-05-05 17:03:50 -0400

    test improvements: rewrite edit() and open() usage with testing mocks

--- SoftwareTests/PreFiles/PreImplementExplicitSolver.m
+++ SoftwareTests/PreFiles/PreImplementExplicitSolver.m
@@ -3,3 +3,9 @@
 KnownIssuesID = "";
 % ---- Pre-run commands -----
  
+open = @(str)MyEdit(str);
+
+function MyEdit(str)
+assert(exist(str,"file"))
+disp("Edit the file " + str)
+end
--- SoftwareTests/PreFiles/PreMethodOfCharacteristics.m
+++ SoftwareTests/PreFiles/PreMethodOfCharacteristics.m
@@ -3,3 +3,9 @@
 KnownIssuesID = "";
 % ---- Pre-run commands -----
  
+edit = @(str)MyEdit(str);
+
+function MyEdit(str)
+assert(exist(str,"file"))
+disp("Edit the file " + str)
+end
